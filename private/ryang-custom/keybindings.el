6d266100741fb3f795be2a93eed2aa7c41f61984
Rui Yang <ryang.sei@gmail.com>
2017-02-27 15:11:10 +1100

    Update keybindings.el
    migrate more keybindings

--- private/ryang-custom/keybindings.el
+++ private/ryang-custom/keybindings.el
@@ -1,6 +1,9 @@
 (global-set-key [f2] 'find-name-dired)
+(global-set-key [f5] 'refresh-file)
 (global-set-key [f6] 'find-grep)
 (global-set-key [f7] 'replace-string)
+(global-set-key [(f12)] 'save-buffers-kill-emacs)
+
 (global-set-key [(control x) (k)] 'kill-this-buffer)
 (global-set-key [(control x) (control y)] 'browse-kill-ring)
 (global-set-key (kbd "C-f") 'forward-word)
@@ -17,3 +20,6 @@
 (global-unset-key "\C-x\C-z")
 
 (global-set-key (kbd "C-x C-f") 'ido-find-file)
+
+(global-set-key (kbd "C-M-=") 'text-scale-increase)
+(global-set-key (kbd "C-M--") 'text-scale-decrease)

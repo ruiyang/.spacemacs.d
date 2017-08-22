d8d5e691a51d818748961455c4c33e9fec43a4ff
Rui Yang <ryang.sei@gmail.com>
2017-08-22 22:34:34 +1000

    refactor to load local custom packages. local packages must be under a folder with the package name

--- private/ryang-custom/packages.el
+++ private/ryang-custom/packages.el
@@ -1,4 +1,12 @@
-(setq ryang-custom-packages
+(defconst ryang-custom-packages
   '(
     (revive :location local)
     ))
+
+(defun ryang-custom/init-revive ()
+  (use-package revive
+    :config
+    (progn
+      (if (file-exists-p "~/.revive.el")
+          (resume)))))
+
